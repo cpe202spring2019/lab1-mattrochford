c71536be41188054983cbd36695e6ed8061b30ff
Matt Rochford <matterochford@gmail.com>
2019-04-08 23:29:50 -0700

    Matt Rochford

--- lab1.py
+++ lab1.py
@@ -1,39 +1,69 @@
 
 def max_list_iter(int_list):  # must use iteration not recursion
-   """finds the max of a list of numbers and returns the value (not the index)
-   If int_list is empty, returns None. If list is None, raises ValueError"""
+    """finds the max of a list of numbers and returns the value (not the index)
+    If int_list is empty, returns None. If list is None, raises ValueError"""
 
-   if None == int_list: # Check for None list
-   	raise ValueError('List contains None element')
+    if None == int_list: # Check for None list
+   	    raise ValueError('List contains None element')
 
-   elif not int_list: # Check for empty list
-   	return(None)
+    elif not int_list: # Check for empty list
+   	    return(None)
 
-   else: # Find max value
-   	max_int = int_list[0] # Initialize first value as max
-   	for i in int_list:
-   		if i > max_int: # Check for new max
-   			max_int = i # Update max
+    else: # Find max value
+   	    max_int = int_list[0] # Initialize first value as max
+   	    for i in int_list:
+   		    if i > max_int: # Check for new max
+   			    max_int = i # Update max
 
-   return(max_int)
+    return(max_int)
 
 def reverse_rec(int_list):   # must use recursion
-   """recursively reverses a list of numbers and returns the reversed list
-   If list is None, raises ValueError"""
+    """recursively reverses a list of numbers and returns the reversed list
+    If list is None, raises ValueError"""
 
-   if None == int_list: # Check for None list
-   	raise ValueError('List contains None element')
+    if None == int_list: # Check for None list
+   	    raise ValueError('List contains None element')   
+
+    elif not int_list: # Check for empty list
+   	    return([])
+
+    else:
+    	return [int_list[-1]] + reverse_rec(int_list[:-1]) # Add last element and call function again
+    
+
 
 
 def bin_search(target, low, high, int_list):  # must use recursion
-   """searches for target in int_list[low..high] and returns index if found
-   If target is not found returns None. If list is None, raises ValueError """
-   pass
+    """searches for target in int_list[low..high] and returns index if found
+    If target is not found returns None. If list is None, raises ValueError """
+
+    if int_list == None: # Check for None list
+   	    raise ValueError('List contains None element')   
+
+    middle = (high+low)//2 # Find middle index
+
+    if (high-low)//2 == 0: # Check if list only has one number left
+    	if target != middle: # If last number doesnt match return empty list
+    		return []
+
+    if target == int_list[middle]: # If target matches middle number
+        return middle
+    elif target > int_list[middle]: # If target is less than middle number
+        return bin_search(target,middle,high,int_list) # Recursive function call
+    else: # If target is greater than middle number
+        return bin_search(target,low,middle,int_list) # Recursive function call
+
+   	# elif low < 0 or not float(low).is_integer(): # Check first search index
+    #     raise ValueError('Low value cannot be less than zero and must be integer')
+
+   	# elif high > len(int_list)-1 or not float(high).is_integer(): # Check first search index
+    #     raise ValueError('Low value cannot be less than zero and must be integer')
 
 
 def main():
+    pass
 
-   pass
+
 
 if __name__ == "__main__":
     main()
--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -4,37 +4,68 @@ from lab1 import *
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
+    def test_bin_search(self):
+
+        print('\nTest #1.1')
+        list_val =[0,1,2,3,4,7,8,9,10]
+        low = 0
+        high = len(list_val)-1
+        self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 ) # Provided test case
+
+        print('Test #1.2')
+        tlist1 = [23,45,46,56,68,76,88,90,111,123,435,4355,9009]
+        self.assertEqual(bin_search(76,0,len(tlist1)-1,tlist1),5) # Test search with result
+
+        print('Test #1.3')
+        self.assertEqual(bin_search(78,0,len(tlist1)-1,tlist1),[]) # Test search with no result
+
+        print('Test #1.4')
+        self.assertEqual(bin_search(4355,0,len(tlist1)-1,tlist1),11) # Test search with result
+
+        print('Test #1.5')
+        with self.assertRaises(ValueError): # Check for none exception
+            bin_search(44,0,12,None)   
+
     def test_max_list_iter(self):
 
         # Test max_list_iter for full functionality
 
-        print('Test #1.1')
+        print('\nTest #2.1')
         tlist1 = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist1)
 
-        print('Test #1.2')
+        print('Test #2.2')
         tlist2 = []
         self.assertEqual(max_list_iter(tlist2),None) # Check empty lists
 
-        print('Test #1.3')
+        print('Test #2.3')
         tlist3 = [0.023,199123,-234233,98.5]
-        self.assertEqual(max_list_iter(tlist3),199123)
+        self.assertEqual(max_list_iter(tlist3),199123) # Check for max value
 
-        print('Test #1.4')
+        print('Test #2.4')
         tlist4 = [0,-23234,-76,-0.00000001]
-        self.assertEqual(max_list_iter(tlist4),0)
+        self.assertEqual(max_list_iter(tlist4),0) # Check for max value with 0 and negatives
 
 
 
-    # def test_reverse_rec(self):
-    #     self.assertEqual(reverse_rec([1,2,3]),[3,2,1])
+    def test_reverse_rec(self):
+        print('\nTest #3.1')
+        self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) # Check for reverse list
 
-    # def test_bin_search(self):
-    #     list_val =[0,1,2,3,4,7,8,9,10]
-    #     low = 0
-    #     high = len(list_val)-1
-    #     self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 )
+        print('Test #3.2')
+        tlist1 = None
+        with self.assertRaises(ValueError):  # used to check for None exception
+            max_list_iter(tlist1)
+
+        print('Test #3.3')
+        self.assertEqual(reverse_rec([]),[]) # Check for empty list functionality
+
+        print('Test #3.4')
+        tlist2 = range(13,21) 
+        self.assertEqual(reverse_rec(tlist2),[20,19,18,17,16,15,14,13]) # Check another longer list
+
+ 
 
 if __name__ == "__main__":
         unittest.main()
