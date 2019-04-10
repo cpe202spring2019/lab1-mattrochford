a78e41a829fd0bb33df761ee6d98e4a755d72659
Matt Rochford <matterochford@gmail.com>
2019-04-10 15:57:25 -0700

    Matt Rochford

--- lab1_test_cases.py
+++ lab1_test_cases.py
@@ -4,64 +4,57 @@ from lab1 import *
  # A few test cases.  Add more!!!
 class TestLab1(unittest.TestCase):
 
-    def test_bin_search(self):
-
-        print('\nTest #1.1')
+    def test0_bin_search(self):
         list_val =[0,1,2,3,4,7,8,9,10]
         low = 0
         high = len(list_val)-1
         self.assertEqual(bin_search(4, 0, len(list_val)-1, list_val), 4 ) # Provided test case
 
-        print('Test #1.2')
+    def test1_bin_search(self):
         tlist1 = [23,45,46,56,68,76,88,90,111,123,435,4355,9009]
         self.assertEqual(bin_search(76,0,len(tlist1)-1,tlist1),5) # Test search with result
 
-        print('Test #1.3')
+    def test2_bin_search(self):
+        tlist1 = [23,45,46,56,68,76,88,90,111,123,435,4355,9009]
         self.assertEqual(bin_search(78,0,len(tlist1)-1,tlist1),[]) # Test search with no result
 
-        print('Test #1.4')
+    def test3_bin_search(self):
+        tlist1 = [23,45,46,56,68,76,88,90,111,123,435,4355,9009]
         self.assertEqual(bin_search(4355,0,len(tlist1)-1,tlist1),11) # Test search with result
 
-        print('Test #1.5')
+    def test4_bin_search(self):
         with self.assertRaises(ValueError): # Check for none exception
             bin_search(44,0,12,None)   
 
-    def test_max_list_iter(self):
-
-        # Test max_list_iter for full functionality
-
-        print('\nTest #2.1')
+    def test0_max_list_iter(self):
         tlist1 = None
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist1)
 
-        print('Test #2.2')
+    def test1_max_list_iter(self):
         tlist2 = []
         self.assertEqual(max_list_iter(tlist2),None) # Check empty lists
 
-        print('Test #2.3')
+    def test2_max_list_iter(self):
         tlist3 = [0.023,199123,-234233,98.5]
         self.assertEqual(max_list_iter(tlist3),199123) # Check for max value
 
-        print('Test #2.4')
+    def test3_max_list_iter(self):
         tlist4 = [0,-23234,-76,-0.00000001]
         self.assertEqual(max_list_iter(tlist4),0) # Check for max value with 0 and negatives
 
-
-
-    def test_reverse_rec(self):
-        print('\nTest #3.1')
+    def test0_reverse_rec(self):
         self.assertEqual(reverse_rec([1,2,3]),[3,2,1]) # Check for reverse list
 
-        print('Test #3.2')
+    def test1_reverse_rec(self):
         tlist1 = None
         with self.assertRaises(ValueError):  # used to check for None exception
             max_list_iter(tlist1)
 
-        print('Test #3.3')
+    def test2_reverse_rec(self):
         self.assertEqual(reverse_rec([]),[]) # Check for empty list functionality
 
-        print('Test #3.4')
+    def test3_reverse_rec(self):
         tlist2 = range(13,21) 
         self.assertEqual(reverse_rec(tlist2),[20,19,18,17,16,15,14,13]) # Check another longer list
 
--- location_tests.py
+++ location_tests.py
@@ -3,16 +3,25 @@ from location import *
 
 class TestLab1(unittest.TestCase):
 
-    def test_repr(self):
+    def test_repr(self): # Test representation function
         loc = Location("SLO", 35.3, -120.7)
         self.assertEqual(repr(loc),"Location('SLO', 35.3, -120.7)")
 
-    def test_eq(self):
-    	loc1 = Location("SLO", 35.3, -120.7)
-    	loc2 = Location("San Luis Obispo", 35.3, -120.7)
-    	self.assertTrue(loc1==loc2)
+    def test_eq(self): # Test equal locations
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("San Luis Obispo", 35.3, -120.7)
+        self.assertTrue(loc1==loc2)
+
+    def test_eq_2(self): # Test equal locations differently
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("San Luis Obispo", 35.3, -120.7)
+        self.assertEqual(loc1,loc2)
+
+    def test_not_eq(self): # Test not equal locations
+        loc1 = Location("SLO", 35.3, -120.7)
+        loc2 = Location("Not_SLO", 36.0, 123)
+        self.assertNotEqual(loc1,loc2)
     
-    # Add more tests!
 
 if __name__ == "__main__":
         unittest.main()
